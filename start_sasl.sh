fffd422daf55d6a4c0e5ec9a505b412dd7b8c261
yongquanlim <derek.yong.quan.lim@gmail.com>
2021-07-24 20:54:39 +0800

    improve by using current directory

--- start_sasl.sh
+++ start_sasl.sh
@@ -1,12 +1,5 @@
-my_kafka_dir="/Users/yongquanlim/Dev/kafka_2.13-2.8.0"
+export KAFKA_OPTS=-Djava.security.auth.login.config=$(pwd)/config/kafka_server_jaas.conf
 
-export KAFKA_OPTS=-Djava.security.auth.login.config=$my_kafka_dir/config/kafka_server_jaas.conf
-
-sed -i .bak "s|\${KAFKA_DIR}|$my_kafka_dir|g" config/server.properties
-sed -i .bak "s|\${KAFKA_DIR}|$my_kafka_dir|g" config/ssl-consumer.properties
-sed -i .bak "s|\${KAFKA_DIR}|$my_kafka_dir|g" config/ssl-producer.properties
-sed -i .bak "s|\${KAFKA_DIR}|$my_kafka_dir|g" config/ssl-user-config.properties
-
-echo "Starting Kafka at directory: "$my_kafka_dir
+echo "KAFKA_OPTS: "$KAFKA_OPTS
 
 ./bin/kafka-server-start.sh ./config/server.properties
