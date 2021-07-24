7d5364d1e8d377b54215ada12b7c51e1879d6067
limyongquan1992 <derek.yong.quan.lim@gmail.com>
2021-07-24 21:06:41 +0800

    Develop (#1)
    * improve path management
    
    * improve by using current directory
    
    * add sample config file for spring-boot

--- start_sasl.sh
+++ start_sasl.sh
@@ -1,3 +1,5 @@
-export KAFKA_OPTS=-Djava.security.auth.login.config=/Users/yongquanlim/Dev/kafka_2.13-2.8.0/config/kafka_server_jaas.conf
+export KAFKA_OPTS=-Djava.security.auth.login.config=$(pwd)/config/kafka_server_jaas.conf
+
+echo "KAFKA_OPTS: "$KAFKA_OPTS
 
 ./bin/kafka-server-start.sh ./config/server.properties
